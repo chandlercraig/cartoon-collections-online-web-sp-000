a05c0d48ee543cbf03d6e43d40ddf58637f48485
Chandler Craig <hellochandlercraig@gmail.com>
2020-11-24 21:08:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,10 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls_arr)
-  calls_arr.any? { |word| word.length >= 4 }
+  if calls_arr.any? { |word| word.length >= 4 }
+    true
+  else
+    false
 end
 
 def find_the_cheese# code an argument here
