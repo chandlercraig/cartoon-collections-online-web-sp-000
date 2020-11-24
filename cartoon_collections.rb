a314369908d53b6946c0d477faffa74590572a8f
Chandler Craig <hellochandlercraig@gmail.com>
2020-11-24 21:08:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls_arr)
-  if calls_arr.any? { |word| word.length >= 4 }
+  if calls_arr.any? { |word| word.length > 4 }
     true
   else
     false
