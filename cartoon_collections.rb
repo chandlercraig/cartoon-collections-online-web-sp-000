df65c5eeb5fabc5ddc5493d958cf613359d1e7f4
Chandler Craig <hellochandlercraig@gmail.com>
2020-11-24 21:10:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def long_planeteer_calls(calls_arr)
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(food_arr)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
